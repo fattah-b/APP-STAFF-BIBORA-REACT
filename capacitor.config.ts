330821cbb0ee9890815d636bd04fe1d413a82b70
Fattah <asdertmozart@gmail.com>
2026-09-21 12:31:11 +0800

    Update bundle ID to com.darsaistiqomah.staffapp

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.staff.app',
+  appId: 'com.darsaistiqomah.staffapp',
   appName: 'Staff App',
   webDir: 'dist'
 };
